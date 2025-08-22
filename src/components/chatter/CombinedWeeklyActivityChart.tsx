99cde6afea437466d01c9c1efb356838e3728262
Abhishek <abhishekkumarshah2.0@gmail.com>
2025-08-22 08:58:37 +0000

    fixed tooltip of combined weekly activity

--- src/components/chatter/CombinedWeeklyActivityChart.tsx
+++ src/components/chatter/CombinedWeeklyActivityChart.tsx
@@ -149,7 +149,7 @@ const CombinedWeeklyActivityChart: React.FC<CombinedWeeklyActivityChartProps> = 
         colorMap,
     };
   }, [messages1, messages2, chatName1, chatName2]);
-  
+  {/* hi */}
   const renderLegend = () => {
     return (
       <div className="flex justify-center mt-4 text-xs gap-x-8">
