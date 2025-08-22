053e903f60ecfeeb35808c309cb3d0913dfddc79
Abhishek <abhishekkumarshah2.0@gmail.com>
2025-08-22 10:35:04 +0000

    same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   appId: "1:705505813131:web:af61bbea57dbcc094e6a8e",
   storageBucket: "chatter-insights-oki29.firebasestorage.app",
   apiKey: "AIzaSyBbcqql-8K1YeiIoXg4RgjdmyvBWehDgio",
-  authDomain: "chatter-insights-oki29.firebaseapp.com",
+  authDomain: "chatter-insights-oki29.web.app",
   measurementId: "",
   messagingSenderId: "705505813131"
 };
@@ -17,4 +17,3 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
-
